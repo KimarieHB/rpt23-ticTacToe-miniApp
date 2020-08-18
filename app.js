d64e0e29a84053c80687fd0cffb8b44ca883f8b9
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-08-18 16:54:16 -0700

    Update message posted after each turn

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 let turn = 1;
-const gamePlay = () => {
-  turn = 1;
-}
 
 const addXO = (event) => {
   let id = event.target.id;
   if (turn % 2 === 1) {
    document.getElementById(id).innerHTML = 'X';
+   document.getElementById('message').innerHTML = 'Now it is O\'s turn!';
   } else {
    document.getElementById(id).innerHTML = '0';
+   document.getElementById('message').innerHTML = 'Time for X to go!';
   }
   turn++;
 }
 
 const gameReset = () => {
-  gamePlay();
+  turn = 1;
   let squares = document.getElementsByClassName('square');
   for (let i = 0; i < 9; i++) {
     let id = squares[i].id;
