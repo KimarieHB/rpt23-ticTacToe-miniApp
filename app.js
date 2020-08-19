9143df3419b8c10521ec4f3efc0403989cfa609d
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-08-18 19:10:53 -0700

    Add css stylesheet, start app.js refactor for separation of concerns

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
-let turn = 1;
+let gameValues = {
+  turn: 1,
+  selectedBoxes: []
+}
 
 const addXO = (event) => {
   let id = event.target.id;
-  if (turn % 2 === 1) {
-   document.getElementById(id).innerHTML = 'X';
-   document.getElementById('message').innerHTML = 'Now it is O\'s turn!';
+
+  if (gameValues.selectedBoxes.indexOf(id) === -1) {
+  
+    if (gameValues.turn % 2 === 1) {
+      addX(id);
+    } else {
+      addO(id);
+    }
+    
   } else {
-   document.getElementById(id).innerHTML = '0';
-   document.getElementById('message').innerHTML = 'Time for X to go!';
+    document.getElementById('message').innerHTML = 'That square is not available! Choose again!';
   }
-  turn++;
+  // All squares are full when turn = 10
 }
 
+const addX = (id) => {
+  document.getElementById(id).innerHTML = 'X';
+  document.getElementById('message').innerHTML = 'Now it is O\'s turn!';
+  gameValues.turn++;
+  gameValues.selectedBoxes.push(id);
+}
+
+const addO = (id) => {
+  document.getElementById(id).innerHTML = '0';
+  document.getElementById('message').innerHTML = 'Time for X to go!';
+  gameValues.turn++;
+  gameValues.selectedBoxes.push(id);
+}
+
+
 const gameReset = () => {
-  turn = 1;
+  gameValues.turn = 1;
+  gameValues.selectedBoxes = [];
   let squares = document.getElementsByClassName('square');
   for (let i = 0; i < 9; i++) {
     let id = squares[i].id;
     document.getElementById(id).innerHTML = '';
   }
   document.getElementById('message').innerHTML = 'X goes first!';
-
 }
